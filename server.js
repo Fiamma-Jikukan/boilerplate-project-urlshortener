a8a3ed20e9076e5f2fc8b769d581bd90f28e33b5
Fiamma no Jikukan <stavtxxx1@gmail.com>
2021-06-17 10:23:00 +0300

    initial setup

--- server.js
+++ server.js
@@ -10,15 +10,27 @@ app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
+// app.use(express.json());
+
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
+app.get('/api/shorturl', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
-app.listen(port, function() {
+app.post('/api/shorturl', function (req, res) {
+  const { name } = req.body
+  if (name) {
+    return res.status(200).json({ name : 'hello ${name}' });
+  }
+  res.status(401).json({ name : "baka"});
+});
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
