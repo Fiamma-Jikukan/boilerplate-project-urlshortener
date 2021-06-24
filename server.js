9b3d5952582b3fdaca8019e036e1a101b3f9d0d2
Fiamma no Jikukan <stavtxxx1@gmail.com>
2021-06-24 11:08:04 +0300

    next time :(

--- server.js
+++ server.js
@@ -1,34 +1,36 @@
 require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser')
 const cors = require('cors');
 const app = express();
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
-app.use(cors());
+mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+const schema = new mongoose.Schema({ url: "string" })
+const url = mongoose.model('url', schema)
 
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cors());
 app.use('/public', express.static(`${process.cwd()}/public`));
 
-// app.use(express.json());
 
-app.use(express.urlencoded({ extended: false }));
+
+// app.use(express.urlencoded({ extended: false }));
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// Your first API endpoint
-app.get('/api/shorturl', function (req, res) {
-  res.json({ greeting: 'hello API' });
-});
 
-app.post('/api/shorturl', function (req, res) {
-  const { name } = req.body
-  if (name) {
-    return res.status(200).json({ name : 'hello ${name}' });
-  }
-  res.status(401).json({ name : "baka"});
+app.post('/api/shorturl', async function (req, res) {
+  console.log(req.body);
+  const url = new url({ url: req.body.url })
+  await url.save((err, data) => {
+    res.json({ created: true })
+  })
 });
 
 app.listen(port, function () {
